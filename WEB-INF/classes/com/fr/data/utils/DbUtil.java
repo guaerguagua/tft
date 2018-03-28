21455c79f0d2274a45d5ebb7f83f7e9488f48fb2
guaerguagua <1025191631@qq.com>
2018-03-28 18:13:47 +0800

    dbutils,cpt file

--- WEB-INF/classes/com/fr/data/utils/DbUtil.java
+++ WEB-INF/classes/com/fr/data/utils/DbUtil.java
@@ -4,21 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbUtil {
-    private static final String URLPREFIX ="jdbc:mysql://88.88.15.11:3306/";
-    private static final String USERNAME ="test";
-    private static final String PASSWORD ="test";
+    private static final String URLFORMAT ="jdbc:mysql://%s:%s/%s";
 
     private static final String ACTDB="tftactdb";
+    private static final String ACTDBIP="88.88.15.11";
+    private static final String ACTDBPORT="3306";
+    private static final String ACTDBUSER="test";
+    private static final String ACTDBPASS="test";
+
     private static final String HISDB="tfthisdb";
+    private static final String HISDBIP="88.88.15.11";
+    private static final String HISDBPORT="3306";
+    private static final String HISDBUSER="test";
+    private static final String HISDBPASS="test";
+
     private static final String TEST ="tfttest";
+    private static final String TESTIP ="88.88.15.11";
+    private static final String TESTPORT ="3306";
+    private static final String TESTUSER ="test";
+    private static final String TESTPASS ="test";
+
     private static final String MGMDB ="tftmgmdb";
+    private static final String MGMDBIP ="88.88.15.11";
+    private static final String MGMDBPORT ="3306";
+    private static final String MGMDBUSER ="test";
+    private static final String MGMDBPASS ="test";
 
-    private static Connection getConnection(String tablename){
+    private static Connection getConnection(String dbname,String username,String password,String ip,String port){
         String driverName = "com.mysql.jdbc.Driver";
-        String url = URLPREFIX+tablename;
-        String username = USERNAME;
-        String password = PASSWORD;
-
+        String url = String.format(URLFORMAT,ip,port,dbname);
         try {
             Class.forName(driverName);
             java.sql.Connection con = DriverManager.getConnection(url, username, password);
@@ -30,18 +44,18 @@ public class DbUtil {
     }
 
     public static Connection getActConnection() {
-        return getConnection(ACTDB);
+        return getConnection(ACTDB,ACTDBUSER,ACTDBPASS,ACTDBIP,ACTDBPORT);
     }
 
     public static Connection getHisConnection() {
-        return getConnection(HISDB);
+        return getConnection(HISDB,HISDBUSER,HISDBPASS,HISDBIP,HISDBPORT);
     }
 
     public static Connection getTestConnection() {
-        return getConnection(TEST);
+        return getConnection(TEST,TESTUSER,TESTPASS,TESTIP,TESTPORT);
     }
 
     public static Connection getMgmConnection() {
-        return getConnection(MGMDB);
+        return getConnection(MGMDB,MGMDBUSER,MGMDBPASS,MGMDBIP,MGMDBPORT);
     }
 }
