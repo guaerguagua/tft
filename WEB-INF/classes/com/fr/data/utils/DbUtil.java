41c0106c00774da5600fc821677d6bd9640c233d
guaerguagua <1025191631@qq.com>
2018-03-28 18:59:39 +0800

    dbutils,cpt file

--- WEB-INF/classes/com/fr/data/utils/DbUtil.java
+++ WEB-INF/classes/com/fr/data/utils/DbUtil.java
@@ -7,28 +7,28 @@ public class DbUtil {
     private static final String URLFORMAT ="jdbc:mysql://%s:%s/%s";
 
     private static final String ACTDB="tftactdb";
-    private static final String ACTDBIP="88.88.15.11";
-    private static final String ACTDBPORT="3306";
-    private static final String ACTDBUSER="test";
-    private static final String ACTDBPASS="test";
+    private static final String ACTDBIP="10.40.2.26";
+    private static final String ACTDBPORT="60002";
+    private static final String ACTDBUSER="act_ap";
+    private static final String ACTDBPASS="OStem@00";
 
     private static final String HISDB="tfthisdb";
-    private static final String HISDBIP="88.88.15.11";
-    private static final String HISDBPORT="3306";
-    private static final String HISDBUSER="test";
-    private static final String HISDBPASS="test";
+    private static final String HISDBIP="10.40.2.26";
+    private static final String HISDBPORT="60003";
+    private static final String HISDBUSER="his_ap";
+    private static final String HISDBPASS="OStem@00";
 
     private static final String TEST ="tfttest";
-    private static final String TESTIP ="88.88.15.11";
-    private static final String TESTPORT ="3306";
+    private static final String TESTIP ="10.40.2.26";
+    private static final String TESTPORT ="60001";
     private static final String TESTUSER ="test";
     private static final String TESTPASS ="test";
 
     private static final String MGMDB ="tftmgmdb";
-    private static final String MGMDBIP ="88.88.15.11";
-    private static final String MGMDBPORT ="3306";
-    private static final String MGMDBUSER ="test";
-    private static final String MGMDBPASS ="test";
+    private static final String MGMDBIP ="10.40.2.26";
+    private static final String MGMDBPORT ="60001";
+    private static final String MGMDBUSER ="mgm_ap";
+    private static final String MGMDBPASS ="OStem@00";
 
     private static Connection getConnection(String dbname,String username,String password,String ip,String port){
         String driverName = "com.mysql.jdbc.Driver";
